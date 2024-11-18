c8d3d23a1a3419bcf123d1036e15e99f7bd460be
Aniket gaikwad <100470741+Aniket17200@users.noreply.github.com>
2024-11-18 14:49:25 +0530

    new chages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Component/Navbar';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Feature from './Pages/Feature';
+import Home from './Pages/Home';
+import Services from './Pages/Services';
+import Login from './Component/Login';
+import Register from './Component/Register';
+import ROapp from './ronkasrc/ROapp';
+import RjApp from './Rsrc2/Rjapp';
+import  RTApp  from './RTSrc3/RTApp';
+import RTHome from './RTSrc3/pages/RTHome';
+import RTAbout from './RTSrc3/pages/RTAbout';
+import RTServices from './RTSrc3/components/RTServices';
+import RTSignUp from './RTSrc3/pages/RTSignUp';
+import RTLogin from './RTSrc3/pages/RTLogin';
+import TjHomePage from './Tjsorce/Tjcomponents/TjHomePage';
+import TjCategories from './Tjsorce/Tjcomponents/TjCategories';
+import TjShopPage from './Tjsorce/Tjcomponents/TJShopPage';
+import TjApp from './Tjsorce/TjApp';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/feature" element={<Feature/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Register/>} />
+        <Route path="/Temp1" element={<ROapp/>} />
+        <Route path="/Temp2" element={<RjApp/>} />
+        <Route path="/Temp3" element={<RTApp/>} />
+        <Route path="/Temp4" element={<TjApp/>} />
+
+        <Route path="/RThome" element={<RTHome/>} />
+        <Route path="/RTabout" element={<RTAbout/>} />
+        <Route path="/RTservices" element={<RTServices/>} />
+        <Route path="/RTsiginup" element={<RTSignUp/>} />
+        <Route path="/RTlogin" element={<RTLogin/>} />
+        <Route path="/Tjhome" element={<TjHomePage/>} />
+        <Route path="/Tjcontact" element={<TjCategories/>} />
+        <Route path="/TjShop" element={<TjShopPage/>} />
+
+    </Routes>
+</Router>
   );
 }
 
